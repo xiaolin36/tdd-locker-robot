ae8b18b57f241a894afc2e5e7793338682a1411e
Xiaolin <xlbian@thoughtworks.com>
2020-03-25 21:26:03 +0800

    Add the implementation codes to make the test should_return_ticket_when_press_open_given_17_available_locker pass.

--- src/main/java/cn/xpbootcamp/gilded_rose/LockerSystem.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/LockerSystem.java
@@ -1,10 +1,17 @@
 package cn.xpbootcamp.gilded_rose;
 
+import java.util.Random;
 import java.util.Set;
 
 class LockerSystem {
   public int pressOpenButton(int lockerCount, Set<String> ticketQueue) {
+    lockerCount = lockerCount - 1;
+    Random random = new Random();
+    String ticket = Integer.toString(random.nextInt());
+    while (ticketQueue.contains(ticket)) {
+      ticket = Integer.toString(random.nextInt());
+    }
+    ticketQueue.add(ticket);
     return lockerCount;
   }
-
 }
