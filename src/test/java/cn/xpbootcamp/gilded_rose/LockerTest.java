7b9157d2c21b370f0699fa58d56701ed24092eb7
Xiaolin <xlbian@thoughtworks.com>
2020-03-25 21:30:46 +0800

    Create the second  test, should_not_return_ticket_when_press_open_given_0_available_locker, it fails.

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -24,4 +24,40 @@ class LockerTest {
     assertEquals((originLockerCount - 1), currentLockerCount);
     assertEquals(3, ticketQueue.size());
   }
+
+
+  @Test
+  void should_not_return_ticket_when_press_open_given_0_available_locker() {
+    // Given
+    int originLockerCount = 0;
+    Set<String> ticketQueue = new HashSet<>();
+    ticketQueue.add("001");
+    ticketQueue.add("002");
+    ticketQueue.add("003");
+    ticketQueue.add("004");
+    ticketQueue.add("005");
+    ticketQueue.add("006");
+    ticketQueue.add("007");
+    ticketQueue.add("008");
+    ticketQueue.add("009");
+    ticketQueue.add("010");
+    ticketQueue.add("011");
+    ticketQueue.add("012");
+    ticketQueue.add("013");
+    ticketQueue.add("014");
+    ticketQueue.add("015");
+    ticketQueue.add("016");
+    ticketQueue.add("017");
+    ticketQueue.add("018");
+    ticketQueue.add("019");
+
+    // When
+    LockerSystem lockerSystem = new LockerSystem();
+    int currentLockerCount = lockerSystem.pressOpenButton(originLockerCount, ticketQueue);
+
+    // Then
+    assertEquals(originLockerCount, currentLockerCount);
+    assertEquals(19, ticketQueue.size());
+  }
+
 }
