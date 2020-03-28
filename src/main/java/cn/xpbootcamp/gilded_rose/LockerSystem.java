732dc3acaa3e717d3372d462bd4960e6781fad93
Xiaolin <xlbian@thoughtworks.com>
2020-03-28 18:29:36 +0800

    Refactor the process with new tasking list. Create the test should_obtain_ticket_when_deposit_bag_given_2_available_spaces, and it fails.

--- src/main/java/cn/xpbootcamp/gilded_rose/LockerSystem.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/LockerSystem.java
@@ -1,28 +1,14 @@
 package cn.xpbootcamp.gilded_rose;
 
-import java.util.Random;
-import java.util.Set;
-
 class LockerSystem {
-  public int pressOpenButton(int lockerCount, Set<String> ticketQueue) {
-    if (lockerCount > 0) {
-      lockerCount = lockerCount - 1;
-      Random random = new Random();
-      String ticket = "0000000000";
-      while (ticketQueue.contains(ticket)) {
-        ticket = Integer.toString(random.nextInt());
-      }
-      ticketQueue.add(ticket);
-    }
+  public LockerSystem(int availableSpaces) {
 
-    return lockerCount;
   }
 
-  public int scanTicket(int lockerCount, Set<String> ticketQueue, String ticketNumber) {
-    if (ticketQueue.contains(ticketNumber)) {
-      lockerCount = lockerCount + 1;
-      ticketQueue.remove(ticketNumber);
-    }
-    return lockerCount;
+  public Ticket depositBag(){
+    return null;
   }
+
 }
+
+
--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -1,96 +1,22 @@
 package cn.xpbootcamp.gilded_rose;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class LockerTest {
-  @Test
-  void should_return_ticket_when_press_open_given_17_available_locker() {
-    // Given
-    int originLockerCount = 17;
-    Set<String> ticketQueue = new HashSet<>();
-    ticketQueue.add("001");
-    ticketQueue.add("002");
+    //  Given 2 available spaces, When deposit the bag, Then deposit succeed and obtained 1 ticket.
+    @Test
+    void should_obtain_ticket_when_deposit_bag_given_2_available_spaces(){
+      // Given
+      int availableSpaces = 2;
+      LockerSystem lockerSystem = new LockerSystem(availableSpaces);
 
-    // When
-    LockerSystem lockerSystem = new LockerSystem();
-    int currentLockerCount = lockerSystem.pressOpenButton(originLockerCount, ticketQueue);
+      // When
+      Ticket ticket = lockerSystem.depositBag();
 
-    // Then
-    assertEquals((originLockerCount - 1), currentLockerCount);
-    assertEquals(3, ticketQueue.size());
-  }
-
-  @Test
-  void should_not_return_ticket_when_press_open_given_0_available_locker() {
-    // Given
-    int originLockerCount = 0;
-    Set<String> ticketQueue = new HashSet<>();
-    ticketQueue.add("001");
-    ticketQueue.add("002");
-    ticketQueue.add("003");
-    ticketQueue.add("004");
-    ticketQueue.add("005");
-    ticketQueue.add("006");
-    ticketQueue.add("007");
-    ticketQueue.add("008");
-    ticketQueue.add("009");
-    ticketQueue.add("010");
-    ticketQueue.add("011");
-    ticketQueue.add("012");
-    ticketQueue.add("013");
-    ticketQueue.add("014");
-    ticketQueue.add("015");
-    ticketQueue.add("016");
-    ticketQueue.add("017");
-    ticketQueue.add("018");
-    ticketQueue.add("019");
-
-    // When
-    LockerSystem lockerSystem = new LockerSystem();
-    int currentLockerCount = lockerSystem.pressOpenButton(originLockerCount, ticketQueue);
-
-    // Then
-    assertEquals(originLockerCount, currentLockerCount);
-    assertEquals(19, ticketQueue.size());
-  }
-
-  @Test
-  void should_release_locker_when_scan_ticket_given_ticket_not_used() {
-    // Given
-    int originLockerCount = 17;
-    Set<String> ticketQueue = new HashSet<>();
-    ticketQueue.add("001");
-    ticketQueue.add("002");
-
-    // When
-    LockerSystem lockerSystem = new LockerSystem();
-    int currentLockerCount = lockerSystem.scanTicket(originLockerCount, ticketQueue, "001");
-
-    // Then
-    assertEquals((originLockerCount + 1), currentLockerCount);
-    assertEquals(1, ticketQueue.size());
-  }
-
-  @Test
-  void should_not_release_locker_when_scan_ticket_given_ticket_used() {
-    // Given
-    int originLockerCount = 17;
-    Set<String> ticketQueue = new HashSet<>();
-    ticketQueue.add("001");
-    ticketQueue.add("002");
-
-    // When
-    LockerSystem lockerSystem = new LockerSystem();
-    int currentLockerCount = lockerSystem.scanTicket(originLockerCount, ticketQueue, "003");
-
-    // Then
-    assertEquals(originLockerCount, currentLockerCount);
-    assertEquals(2, ticketQueue.size());
-  }
+      // Then
+      assertNotNull(ticket);
+    }
 
 }
