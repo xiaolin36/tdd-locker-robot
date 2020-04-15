bdb3f622f7e16c0734734ed6ae49f2a00cdf2e2e
Xiaolin <xlbian@thoughtworks.com>
2020-04-15 21:17:32 +0800

    Refactor the PrimaryLockerRobot to extends class Robot.

--- src/main/java/cn/xpbootcamp/gilded_rose/PrimaryLockerRobot.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/PrimaryLockerRobot.java
@@ -1,26 +1,19 @@
 package cn.xpbootcamp.gilded_rose;
 
-import cn.xpbootcamp.gilded_rose.exception.InvalidTicketException;
 import cn.xpbootcamp.gilded_rose.exception.NoAvailableSpaceException;
 import cn.xpbootcamp.gilded_rose.model.Bag;
 import cn.xpbootcamp.gilded_rose.model.Locker;
 import cn.xpbootcamp.gilded_rose.model.Ticket;
 
-import java.util.Comparator;
 import java.util.List;
 
-public class PrimaryLockerRobot {
-  private List<Locker> lockers;
+public class PrimaryLockerRobot extends Robot {
 
   public PrimaryLockerRobot(List<Locker> lockers) {
-    this.lockers = sortLockersByIndex(lockers);
+    super(lockers);
   }
 
-  private List<Locker> sortLockersByIndex(List<Locker> lockers) {
-    lockers.sort(Comparator.comparingInt(Locker::getIndex));
-    return lockers;
-  }
-
+  @Override
   public Ticket depositBag(Bag bag) {
     Locker locker = findDepositedLocker();
     if (locker != null) {
@@ -32,23 +25,6 @@ public class PrimaryLockerRobot {
   private Locker findDepositedLocker() {
     for (Locker locker : this.lockers) {
       if (locker.getAvailableSpaces() > 0) {
-        return locker;
-      }
-    }
-    return null;
-  }
-
-  public Bag claimBag(Ticket ticket) {
-    Locker locker = findClaimedLocker(ticket);
-    if (locker != null) {
-      return locker.claimBag(ticket);
-    }
-    throw new InvalidTicketException();
-  }
-
-  private Locker findClaimedLocker(Ticket ticket) {
-    for (Locker locker : this.lockers) {
-      if (locker.getStoredBags().containsKey(ticket.getId())) {
         return locker;
       }
     }
