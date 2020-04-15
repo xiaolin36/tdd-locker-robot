3ca92cf6e6a583e6b741d885932cf0f29e57a428
Xiaolin <xlbian@thoughtworks.com>
2020-04-15 21:15:33 +0800

    Refactor the SmartLockerRobot to extends class Robot.

--- src/main/java/cn/xpbootcamp/gilded_rose/SmartLockerRobot.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/SmartLockerRobot.java
@@ -1,22 +1,20 @@
 package cn.xpbootcamp.gilded_rose;
 
-import cn.xpbootcamp.gilded_rose.exception.InvalidTicketException;
 import cn.xpbootcamp.gilded_rose.model.Bag;
 import cn.xpbootcamp.gilded_rose.model.Locker;
 import cn.xpbootcamp.gilded_rose.model.Ticket;
 
 import java.util.*;
 
-public class SmartLockerRobot {
-  private List<Locker> lockers;
-
+public class SmartLockerRobot extends Robot {
   public SmartLockerRobot(List<Locker> lockers) {
-    this.lockers = sortLockersByIndex(lockers);
+    super(lockers);
   }
 
-  private List<Locker> sortLockersByIndex(List<Locker> lockers) {
-    lockers.sort(Comparator.comparingInt(Locker::getIndex));
-    return lockers;
+  @Override
+  public Ticket depositBag(Bag bag) {
+    Locker locker = findDepositedLocker();
+    return locker.depositBag(bag);
   }
 
   private Locker findDepositedLocker() {
@@ -29,25 +27,4 @@ public class SmartLockerRobot {
     return lockerWithMaxAvailableSpaces;
   }
 
-  public Ticket depositBag(Bag bag) {
-    Locker locker = findDepositedLocker();
-    return locker.depositBag(bag);
-  }
-
-  public Bag claimBag(Ticket ticket) {
-    Locker locker = findClaimedLocker(ticket);
-    if (locker != null) {
-      return locker.claimBag(ticket);
-    }
-    throw new InvalidTicketException();
-  }
-
-  private Locker findClaimedLocker(Ticket ticket) {
-    for(Locker locker: this.lockers) {
-      if (locker.getStoredBags().containsKey(ticket.getId())) {
-        return locker;
-      }
-    }
-    return null;
-  }
 }
